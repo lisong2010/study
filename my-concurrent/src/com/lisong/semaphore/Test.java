0eaeea57da4fc9b6b04dbc91982e4a5a89c1c25a
lisong <lisong@qianmi.com>
2018-05-25 18:09:22 +0800

    .

---
+++ my-concurrent/src/com/lisong/semaphore/Test.java
@@ -0,0 +1,33 @@
+package com.lisong.semaphore;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *  信号量
+ *  当现成运行数等于信号量数量时，其他现成等待
+ * @author lisong(OF2016)
+ *         company qianmi.com
+ *         Date    2018-05-25
+ */
+public class Test {
+
+    public static void main(String[] args) throws InterruptedException {
+        Semaphore semaphore = new Semaphore(3);
+
+        for (int i = 0; i < 10; i++) {
+            final int j = i;
+            new Thread(() -> {
+                try {
+                    semaphore.acquire();
+                    System.out.println(String.format("start ...%s, %s", j, System.currentTimeMillis()));
+                    TimeUnit.SECONDS.sleep(2);
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(String.format("end ...%s, %s", j, System.currentTimeMillis()));
+            }).start();
+        }
+    }
+}
